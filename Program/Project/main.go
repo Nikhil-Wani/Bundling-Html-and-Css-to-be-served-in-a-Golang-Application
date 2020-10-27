12fa92c02af9096d17fb14a0087aa9104892530c
NIKHIL WANI <65514201+Nikhil-Wani@users.noreply.github.com>
2020-10-27 21:02:53 +0530

    Add files via upload

---
+++ Program/Project/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"net/http"
+
+	rice "github.com/GeertJohan/go.rice"
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	router := mux.NewRouter()
+	router.PathPrefix("/").Handler(http.FileServer(rice.MustFindBox("portfolio").HTTPBox()))
+	http.ListenAndServe(":1234", router)
+}
